9372fead0814db2518a8082fa30db5f1adf23cae
algobardo <mezzetti@gmail.com>
2016-01-18 17:09:02 +0100

    Desugared

--- web/spirodraw.dart
+++ web/spirodraw.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
 import 'dart:math' as Math;
 
@@ -12,12 +8,9 @@ void main() {
 class Spirodraw {
   static double PI2 = Math.PI * 2;
   Document doc;
-  // Scale factor used to scale wheel radius from 1-10 to pixels
   int RUnits, rUnits, dUnits;
-  // Fixed radius, wheel radius, pen distance in pixels
   double R, r, d;
-  InputElement fixedRadiusSlider, wheelRadiusSlider,
-    penRadiusSlider, penWidthSlider, speedSlider;
+  InputElement fixedRadiusSlider, wheelRadiusSlider, penRadiusSlider, penWidthSlider, speedSlider;
   SelectElement inOrOut;
   DivElement mainDiv;
   num lastX, lastY;
@@ -35,7 +28,6 @@ class Spirodraw {
   int numPoints;
   double speed;
   bool run;
-
   Spirodraw() {
     doc = window.document;
     inOrOut = doc.querySelector("#in_out");
@@ -52,18 +44,18 @@ class Spirodraw {
     paletteElement = doc.querySelector("#palette");
     initControlPanel();
   }
-
   void go() => _setupSize();
-
   void _setupSize() {
     height = window.innerHeight;
     width = window.innerWidth - 270;
     yc = height ~/ 2;
     xc = width ~/ 2;
-    frontCanvas..height = height
-               ..width = width;
-    backCanvas..height = height
-              ..width = width;
+    frontCanvas
+      ..height = height
+      ..width = width;
+    backCanvas
+      ..height = height
+      ..width = width;
     clear();
   }
 
@@ -74,10 +66,8 @@ class Spirodraw {
     speedSlider.onChange.listen(onSpeedChange);
     penRadiusSlider.onChange.listen((_) => refresh());
     penWidthSlider.onChange.listen(onPenWidthChange);
-
     colorPicker = new ColorPicker(paletteElement);
     colorPicker.addListener((String color) => onColorChange(color));
-
     doc.querySelector("#start").onClick.listen((_) => start());
     doc.querySelector("#stop").onClick.listen((_) => stop());
     doc.querySelector("#clear").onClick.listen((_) => clear());
@@ -100,19 +90,15 @@ class Spirodraw {
 
   void refresh() {
     stop();
-    // Reset
     lastX = lastY = 0;
-    // Compute fixed radius
-    // based on starting diameter == min / 2, fixed radius == 10 units
     int min = Math.min(height, width);
     double pixelsPerUnit = min / 40;
     RUnits = fixedRadiusSlider.valueAsNumber.toInt();
     R = RUnits * pixelsPerUnit;
-    // Scale inner radius and pen distance in units of fixed radius
     rUnits = wheelRadiusSlider.valueAsNumber.toInt();
-    r = rUnits * R/RUnits * int.parse(inOrOut.value);
+    r = rUnits * R / RUnits * int.parse(inOrOut.value);
     dUnits = penRadiusSlider.valueAsNumber.toInt();
-    d = dUnits * R/RUnits;
+    d = dUnits * R / RUnits;
     numPoints = calcNumPoints();
     maxTurns = calcTurns();
     onSpeedChange(null);
@@ -121,23 +107,21 @@ class Spirodraw {
   }
 
   int calcNumPoints() {
-    // Empirically, treat it like an oval.
     if (dUnits == 0 || rUnits == 0) return 2;
-
     int gcf_ = gcf(RUnits, rUnits);
     int n = RUnits ~/ gcf_;
     int d_ = rUnits ~/ gcf_;
     if (n % 2 == 1) return n;
-    if (d_% 2 == 1) return n;
-    return n~/2;
+    if (d_ % 2 == 1) return n;
+    return n ~/ 2;
   }
 
   double calcStepSize() => speed / 100 * maxTurns / numPoints;
-
   void drawFrame(double theta) {
     if (animationEnabled) {
-      front..clearRect(0, 0, width, height)
-           ..drawImage(backCanvas, 0, 0);
+      front
+        ..clearRect(0, 0, width, height)
+        ..drawImage(backCanvas, 0, 0);
       drawFixed();
     }
     drawWheel(theta);
@@ -145,7 +129,7 @@ class Spirodraw {
 
   void animate(double time) {
     if (run && rad <= maxTurns * PI2) {
-      rad+=stepSize;
+      rad += stepSize;
       drawFrame(rad);
       window.requestAnimationFrame(animate);
     } else {
@@ -161,20 +145,18 @@ class Spirodraw {
   }
 
   int calcTurns() {
-    // compute ratio of wheel radius to big R then find LCM
-    if ((dUnits==0) || (rUnits==0)) return 1;
+    if ((dUnits == 0) || (rUnits == 0)) return 1;
     int ru = rUnits.abs();
     int wrUnits = RUnits + rUnits;
-    int g = gcf (wrUnits, ru);
+    int g = gcf(wrUnits, ru);
     return ru ~/ g;
   }
 
   void stop() {
     run = false;
-    // Show drawing only
-    front..clearRect(0, 0, width, height)
-         ..drawImage(backCanvas, 0, 0);
-    // Reset angle
+    front
+      ..clearRect(0, 0, width, height)
+      ..drawImage(backCanvas, 0, 0);
     rad = 0.0;
   }
 
@@ -186,78 +168,68 @@ class Spirodraw {
 
   void drawFixed() {
     if (animationEnabled) {
-      front..beginPath()
-           ..lineWidth = 2
-           ..strokeStyle = "gray"
-           ..arc(xc, yc, R, 0, PI2, true)
-           ..closePath()
-           ..stroke();
+      front
+        ..beginPath()
+        ..lineWidth = 2
+        ..strokeStyle = "gray"
+        ..arc(xc, yc, R, 0, PI2, true)
+        ..closePath()
+        ..stroke();
     }
   }
 
-  /**
-   * Draw the wheel with its center at angle theta with respect to the fixed
-   * wheel
-   */
   void drawWheel(double theta) {
     double wx = xc + ((R + r) * Math.cos(theta));
     double wy = yc - ((R + r) * Math.sin(theta));
     if (animationEnabled) {
-      if (rUnits>0) {
-        // Draw ring
-        front..beginPath()
-             ..arc(wx, wy, r.abs(), 0, PI2, true)
-             ..closePath()
-             ..stroke();
-        // Draw center
-        front..lineWidth = 1
-             ..beginPath()
-             ..arc(wx, wy, 3, 0, PI2, true)
-             ..fillStyle = "black"
-             ..fill()
-             ..closePath()
-             ..stroke();
+      if (rUnits > 0) {
+        front
+          ..beginPath()
+          ..arc(wx, wy, r.abs(), 0, PI2, true)
+          ..closePath()
+          ..stroke();
+        front
+          ..lineWidth = 1
+          ..beginPath()
+          ..arc(wx, wy, 3, 0, PI2, true)
+          ..fillStyle = "black"
+          ..fill()
+          ..closePath()
+          ..stroke();
       }
     }
     drawTip(wx, wy, theta);
   }
 
-  /**
-   * Draw a rotating line that shows the wheel rolling and leaves the pen trace
-   *
-   * [wx]    - X coordinate of wheel center
-   * [wy]    - Y coordinate of wheel center
-   * [theta] -  Angle of wheel center with respect to fixed circle
-   */
   void drawTip(double wx, double wy, double theta) {
-    // Calc wheel rotation angle
-    double rot = (r==0) ? theta : theta * (R+r) / r;
-    // Find tip of line
+    double rot = (r == 0) ? theta : theta * (R + r) / r;
     double tx = wx + d * Math.cos(rot);
     double ty = wy - d * Math.sin(rot);
     if (animationEnabled) {
-      front..beginPath()
-           ..fillStyle = penColor
-           ..arc(tx, ty, penWidth/2+2, 0, PI2, true)
-           ..fill()
-           ..moveTo(wx, wy)
-           ..strokeStyle = "black"
-           ..lineTo(tx, ty)
-           ..closePath()
-           ..stroke();
+      front
+        ..beginPath()
+        ..fillStyle = penColor
+        ..arc(tx, ty, penWidth / 2 + 2, 0, PI2, true)
+        ..fill()
+        ..moveTo(wx, wy)
+        ..strokeStyle = "black"
+        ..lineTo(tx, ty)
+        ..closePath()
+        ..stroke();
     }
     drawSegmentTo(tx, ty);
   }
 
   void drawSegmentTo(double tx, double ty) {
     if (lastX > 0) {
-      back..beginPath()
-          ..strokeStyle = penColor
-          ..lineWidth = penWidth
-          ..moveTo(lastX, lastY)
-          ..lineTo(tx, ty)
-          ..closePath()
-          ..stroke();
+      back
+        ..beginPath()
+        ..strokeStyle = penColor
+        ..lineWidth = penWidth
+        ..moveTo(lastX, lastY)
+        ..lineTo(tx, ty)
+        ..closePath()
+        ..stroke();
     }
     lastX = tx;
     lastY = ty;
@@ -267,20 +239,24 @@ class Spirodraw {
 int gcf(int n, int d) {
   if (n == d) return n;
   int max = Math.max(n, d);
-
   for (int i = max ~/ 2; i > 1; i--) {
     if ((n % i == 0) && (d % i == 0)) return i;
   }
-
   return 1;
 }
 
 typedef void PickerListener(String selectedColor);
 
 class ColorPicker {
-  static const hexValues = const ['00', '33', '66', '99', 'CC', 'FF'];
+  static const hexValues = const [
+    '00',
+    '33',
+    '66',
+    '99',
+    'CC',
+    'FF'
+  ];
   static const COLS = 18;
-  // Block height, width, padding
   static const BH = 10;
   static const BW = 10;
   static const BP = 1;
@@ -290,19 +266,15 @@ class ColorPicker {
   final height = 160;
   final width = 180;
   CanvasRenderingContext2D ctx;
-
   ColorPicker(this.canvasElement) : _listeners = [] {
     ctx = canvasElement.context2D;
     drawPalette();
     addHandlers();
     showSelected();
   }
-
   String get selectedColor => _selectedColor;
-
   void set selectedColor(String color) {
     _selectedColor = color;
-
     showSelected();
     fireSelected();
   }
@@ -310,11 +282,11 @@ class ColorPicker {
   void onMouseMove(MouseEvent event) {
     int x = event.offset.x;
     int y = event.offset.y - 40;
-    if (( y < 0) || (x >= width)) {
+    if ((y < 0) || (x >= width)) {
       return;
     }
     ctx.fillStyle = getHexString(getColorIndex(x, y));
-    ctx.fillRect(0, 0, width/2, 30);
+    ctx.fillRect(0, 0, width / 2, 30);
   }
 
   void onMouseDown(MouseEvent event) {
@@ -327,9 +299,6 @@ class ColorPicker {
     selectedColor = getHexString(getColorIndex(x, y));
   }
 
-  /**
-   * Adds a [PickerListener] to receive updates.
-   */
   void addListener(PickerListener listener) {
     _listeners.add(listener);
   }
@@ -340,10 +309,10 @@ class ColorPicker {
   }
 
   void drawPalette() {
-    int i=0;
-    for (int r=0; r < 256; r+=51) {
-      for (int g=0; g < 256; g+=51) {
-        for (int b=0; b < 256; b+=51) {
+    int i = 0;
+    for (int r = 0; r < 256; r += 51) {
+      for (int g = 0; g < 256; g += 51) {
+        for (int b = 0; b < 256; b += 51) {
           String color = getHexString(i);
           ctx.fillStyle = color;
           int x = BW * (i % COLS);
@@ -362,7 +331,6 @@ class ColorPicker {
   }
 
   int getColorIndex(int x, int y) {
-    // Get color index 0-215 using row, col
     int i = y ~/ BH * COLS + x ~/ BW;
     return i;
   }
@@ -376,11 +344,9 @@ class ColorPicker {
 
   String getHexString(num value) {
     int i = value.floor().toInt();
-
     int r = (i ~/ 36) % 6;
     int g = (i % 36) ~/ 6;
     int b = i % 6;
-
     return '#${hexValues[r]}${hexValues[g]}${hexValues[b]}';
   }
 }
